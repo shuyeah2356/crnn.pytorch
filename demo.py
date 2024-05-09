45efdffa3606016bca6895fbadd7acf11bedcab4
树叶 <@LAPTOP-G7EG95S7>
2024-05-09 15:07:06 +0800

    update

--- demo.py
+++ demo.py
@@ -25,25 +25,28 @@ model.load_state_dict(torch.load(model_path, map_location='cpu'))
 converter = utils.strLabelConverter(alphabet, ignore_case=False)
 
 # 图像大小转换器
-transformer = dataset.resizeNormalize((100, 32))
+transformer = dataset.resizeNormalize((100, 32))    
 
 # 读取并转换图像大小为100 x 32 w x h
-image = Image.open(img_path).convert('L')
-image = transformer(image)
+image = Image.open(img_path).convert('L')   #图片转化为单通道灰度图
+image = transformer(image)  # 对于输入的图片转换为相同大小
 
 if torch.cuda.is_available():
     image = image.cuda()
-image = image.view(1, *image.size())  # (b, c, h, w) (1, 1, 32, 100)
+image = image.view(1, *image.size())  # (b, c, h, w) (1, 1, 32, 100)增加batch_size
 print(image.size())
-
+# eval()
 with torch.no_grad():
     model.eval()
     preds = model(image)  # (w c nclass) (26, 1, 37) 26为ctc生成路径长度也是传入rnn的时间步长，1是batchsize，37是字符类别数
 
 _, preds = preds.max(2)  # 取可能性最大的indecis size (26, 1)
-preds = preds.transpose(1, 0).contiguous().view(-1)  # 转成以为索引列表
+print(preds, "preds")
+preds = preds.transpose(1, 0).contiguous().view(-1)  # 转成一维索引列表
+print(preds)
 # 转成字符序列
 preds_size = torch.IntTensor([preds.size(0)])
+# CTC解码
 raw_pred = converter.decode(preds.data, preds_size.data, raw=True)
 sim_pred = converter.decode(preds.data, preds_size.data, raw=False)
 print('%-20s => %-20s' % (raw_pred, sim_pred))
